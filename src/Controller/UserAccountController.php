d37eceaa580c291e427fe86e2c93329392112755
Jonathan Leprini <leprini.jonathan@protonmail.com>
2024-06-21 15:20:55 +0200

    Création de l'espace utilisateur + lien d'activation et désactivation API

--- src/Controller/UserAccountController.php
+++ src/Controller/UserAccountController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,6 +12,11 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserAccountController extends AbstractController
 {
+    public function __construct(private readonly EntityManagerInterface $entityManager,
+                                private readonly UserRepository $userRepository)
+    {
+    }
+
     #[Route('/account', name: 'account.show')]
     #[IsGranted("ROLE_USER")]
     public function index(): Response
@@ -16,5 +24,39 @@ class UserAccountController extends AbstractController
         return $this->render('user_account/index.html.twig', [
             'controller_name' => 'UserAccountController',
         ]);
+    }
+
+    #[Route('/account/api/enabled', name: 'api.enabled')]
+    #[IsGranted("ROLE_USER")]
+    public function enabledApiAccess(): Response
+    {
+        $user = $this->userRepository->find($this->getUser()->getId());
+
+        if (!$user) {
+            return $this->redirectToRoute('account.show');
+        }
+
+        $user->setApiActivated(true);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('account.show');
+    }
+
+    #[Route('/account/api/disabled', name: 'api.disabled')]
+    #[IsGranted("ROLE_USER")]
+    public function disabledApiAccess(): Response
+    {
+        $user = $this->userRepository->find($this->getUser()->getId());
+
+        if (!$user) {
+            return $this->redirectToRoute('account.show');
+        }
+
+        $user->setApiActivated(false);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('account.show');
     }
 }
