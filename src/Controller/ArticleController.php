43f8b253a407316e40fb9d6086773a914e02f9aa
Jonathan Leprini <leprini.jonathan@protonmail.com>
2024-06-24 14:34:24 +0200

    Création de la logique d'ajout et du modification d'un article du panier + déplacement de la logique de vérification si un article est présent de le panier lors de la consultation de sa page détails, déplacement vers l'ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Cart;
+use App\Entity\User;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ArticleController extends AbstractController
 {
@@ -16,22 +19,70 @@ class ArticleController extends AbstractController
     #[Route('/article/{id}', name: 'product.show', defaults: ['id' => -1])]
     public function index(int $id): Response
     {
-        $articles = $this->articleRepository->findBy([], ['name' => 'ASC']);
-
         if ($id === -1) {
+            $articles = $this->articleRepository->findBy([], ['name' => 'ASC']);
+
             return $this->render('product/index.html.twig', [
                 'articles' => $articles,
             ]);
         }
 
         $article = $this->articleRepository->find($id);
-
         if ($article === null) {
             return $this->redirectToRoute('product.show');
         }
 
+        $articleInCart = false;
+        $articleQuantity = 1;
+
+        if ($this->isGranted('ROLE_USER')) {
+            $articleQuantity = $this->articleIsInCart($id);
+
+            if ($articleQuantity) {
+                $articleInCart = true;
+            }
+        }
+
         return $this->render('product/details.html.twig', [
             'article' => $article,
+            'articleInCart' => $articleInCart,
+            'articleQuantity' => $articleQuantity,
         ]);
+    }
+
+    #[IsGranted("ROLE_USER")]
+    public function articleIsInCart(int $articleId): int|false
+    {
+        $user = $this->getUser();
+
+        if (!$user instanceof User) {
+            $this->redirectToRoute('app_login');
+        }
+
+        $cart = $user->getCart();
+
+        if (!$cart instanceof Cart) {
+            return false;
+        }
+
+        $cartItems = $cart->getCartItem();
+
+        if ($cartItems->isEmpty()) {
+            return false;
+        }
+        
+        $articleInCart = false;
+        $articleQuantity = 0;
+
+        foreach ($cartItems as $cartItem) {
+            $article = $cartItem->getArticle();
+
+            if ($article !== null && $article->getId() === $articleId) {
+                $articleInCart = true;
+                $articleQuantity = $cartItem->getQuantity();
+            }
+        }
+
+        return $articleInCart ? $articleQuantity : false;
     }
 }
--- src/Controller/UserCartController.php
+++ src/Controller/UserCartController.php
@@ -2,19 +2,131 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Cart;
+use App\Entity\CartItem;
+use App\Entity\User;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserCartController extends AbstractController
 {
+    public function __construct(private readonly ArticleRepository $articleRepository, private readonly EntityManagerInterface $entityManagerInterface)
+    {
+    }
+
     #[Route('/panier', name: 'cart.show')]
     #[IsGranted("ROLE_USER")]
     public function index(): Response
     {
-        return $this->render('user_cart/index.html.twig', [
-            'controller_name' => 'UserCartController',
-        ]);
+        return $this->render('user_cart/index.html.twig', ['controller_name' => 'UserCartController',]);
+    }
+
+    #[Route('/panier/add/{id}', name: 'cart.add')]
+    #[IsGranted("ROLE_USER")]
+    public function addItemToCart(Request $request, int $id, int $quantity): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user instanceof User) {
+            $this->redirectToRoute('app_login');
+        }
+
+        $cart = $user->getCart();
+
+        if (!$cart) {
+            $cart = new Cart();
+            $cart->setCreatedAt(new \DateTimeImmutable());
+            $user->setCart($cart);
+        }
+
+        $article = $this->articleRepository->find($id);
+
+        $cartItem = new CartItem();
+        $cartItem->setArticle($article);
+        $cartItem->setQuantity($quantity);
+        $cartItem->setCart($cart);
+
+        $cart->addCartItem($cartItem);
+
+        $this->entityManagerInterface->persist($cartItem);
+        $this->entityManagerInterface->persist($cart);
+        $this->entityManagerInterface->persist($user);
+
+        $this->entityManagerInterface->flush();
+
+        return $this->redirectToRoute('cart.show');
+    }
+
+    #[Route('/panier/update/{id}', name: 'cart.update')]
+    #[IsGranted("ROLE_USER")]
+    public function updateItemToCart(Request $request, int $id): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof User) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $cart = $user->getCart();
+        if (!$cart) {
+            throw $this->createNotFoundException('Cart not found');
+        }
+
+        $article = $this->articleRepository->find($id);
+        if (!$article instanceof Article) {
+            throw $this->createNotFoundException('Article not found');
+        }
+
+        $userQuantity = $request->request->get('quantity');
+        $quantityChecked = $this->checkQuantity($id, $userQuantity);
+
+        foreach ($cart->getCartItem() as $cartItem) {
+            $itemArticle = $cartItem->getArticle();
+            // Item du panier non valide, supprimer l'élément non valide
+            if($itemArticle === null) {
+                $cart->removeCartItem($cartItem);
+                $this->entityManagerInterface->remove($cartItem);
+                $this->entityManagerInterface->flush();
+                continue;
+            }
+
+            if ($itemArticle->getId() === $article->getId()) {
+                $cartItem->setQuantity($quantityChecked);
+
+                $this->entityManagerInterface->persist($cartItem);
+                $this->entityManagerInterface->flush();
+
+                break;
+            }
+        }
+
+        return $this->redirectToRoute('cart.show');
+    }
+
+
+    public function checkQuantity(int $id, int $quantityUser): int
+    {
+        $article = $this->articleRepository->find($id);
+
+        if (!$article instanceof Article) {
+            throw $this->createNotFoundException('Article not found');
+        }
+
+        $articleQuantity = $article->getStock();
+
+        if ($quantityUser > $articleQuantity) {
+            return $articleQuantity;
+        }
+
+        if ($quantityUser < 1) {
+            return 0;
+        }
+
+        return $quantityUser;
     }
 }
