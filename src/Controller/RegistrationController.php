9c69a9cd7aa73a571264d4035cb76909dc123fae
Jonathan Leprini <leprini.jonathan@protonmail.com>
2024-06-26 17:32:18 +0200

    Modification de RegistrationController pour connecter automatiquement l'utilisateur après son inscription réussite

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,19 +6,28 @@ use App\Entity\Cart;
 use App\Entity\User;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class RegistrationController extends AbstractController
 {
     public function __construct(private readonly EntityManagerInterface $entityManager,
-                                private readonly UserPasswordHasherInterface $userPasswordHasher)
+                                private readonly UserPasswordHasherInterface $userPasswordHasher,
+                                private readonly RequestStack $requestStack)
     {
     }
 
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
     #[Route('/registration', name: 'registration.show')]
     public function index(Request $request): Response
     {
@@ -39,6 +48,11 @@ class RegistrationController extends AbstractController
             $this->entityManager->persist($cart);
             $this->entityManager->persist($user);
             $this->entityManager->flush();
+
+            // Connecte automatiquement l'utilisateur après son inscription
+            $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+            $this->container->get('security.token_storage')->setToken($token);
+            $this->requestStack->getSession()->set('_security_main', serialize($token));
 
             return $this->redirectToRoute('app_login');
         }
