40e066a9b7f44517e1df9976b62559e122f2694c
Jonathan Leprini <leprini.jonathan@protonmail.com>
2024-06-27 15:01:19 +0200

    Mise en place de la logique du controller ApiSecurityController pour controller l'accès à l'API selon si l'utilisateur à activé son accès ou non

--- src/Security/ApiSecurityController.php
+++ src/Security/ApiSecurityController.php
@@ -2,18 +2,37 @@
 
 namespace App\Security;
 
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 
-class ApiSecurityController implements AuthenticationSuccessHandlerInterface
+readonly class ApiSecurityController implements AuthenticationSuccessHandlerInterface
 {
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token): ?Response
+    public function __construct(private JWTTokenManagerInterface $JWTTokenManager)
     {
-        return new JsonResponse([
-            'test' => 'test',
-        ]);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): JsonResponse
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            $data = ['error' => 'Identifiants incorrects'];
+            $statusCode = Response::HTTP_UNAUTHORIZED;
+        } elseif (!$user->isApiActivated()) {
+            $data = ['error' => 'Accès API non activé'];
+            $statusCode = Response::HTTP_FORBIDDEN;
+        } else {
+            $jwtToken = $this->JWTTokenManager->create($user);
+
+            $data = ['token' => $jwtToken];
+            $statusCode = Response::HTTP_OK;
+        }
+
+        return new JsonResponse($data, $statusCode);
     }
 }
