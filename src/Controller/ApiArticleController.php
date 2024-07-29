06967ff7dc3bd33989586f7ec8fad3b53657a2ba
Jonathan Leprini <leprini.jonathan@protonmail.com>
2024-07-29 17:45:08 +0200

    Ajout de la documentation avec Nelmio API

--- src/Controller/ApiArticleController.php
+++ src/Controller/ApiArticleController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 
 class ApiArticleController extends AbstractController
 {
@@ -18,6 +21,15 @@ class ApiArticleController extends AbstractController
 
 
     #[Route('/api/products', name: 'products', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Retourne la liste des produits',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: Article::class))
+        )
+    )]
+    #[OA\Tag(name: 'Produit')]
     public function getAllProducts(): JsonResponse
     {
         $products = $this->articleRepository->findAll();
